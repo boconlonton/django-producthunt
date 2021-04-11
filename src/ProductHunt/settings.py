17ab751617db0d473a483121a271e8664ba9021f
boconlonton <boconlonton111@gmail.com>
2021-04-11 15:58:20 +0700

    Add - base template - base.html, Add - Homepage

--- src/ProductHunt/settings.py
+++ src/ProductHunt/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'ProductHunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ProductHunt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,5 +126,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ProductHunt/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
