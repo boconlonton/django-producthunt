cc9991fef7c8ababa1630f76bff23e103a73a6f6
boconlonton <boconlonton111@gmail.com>
2021-04-11 17:49:43 +0700

    Add - Templates - Account/signup,signin,signout

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        # User wants to sign up
+        if request.POST['password1'] \
+            == request.POST['password1']:
+                User.objects.get(
+                    username=request.POST['username'])
+    else:
+        # User wants to enter info
+        pass
+    return render(request, "accounts/signup.html")
+
+
+def login(request):
+    return render(request, "accounts/login.html")
+
+
+def logout(request):
+    # TODO: Need to route to hompage
+    # and don't forget to logout.
+    return render(request, "accounts/logout.html")
